98568c93d98b6d90927905aebc0ae39527bf1b97
david <89749147+analnyroztahovac@users.noreply.github.com>
2022-09-05 13:13:57 +0200

    Update funkcii
    Pridane nove funkcie, ako aj overovanie platnosti zadanej IPcky

--- Off-topic/ip_lookup.py
+++ Off-topic/ip_lookup.py
@@ -1,6 +1,14 @@
 # Pomocou requests lib mozeme ziskavat odpovede z web APIs
 import requests
 
+# Pomocou socket lib mozeme "overovat" platnost IPciek
+import socket
+
+# Pomocou sys lib ukoncime program
+import sys
+
+
+# +++ Funkcia pre IP lookup
 def get_location(adresa):
     
     # Premenime adresu na string
@@ -19,7 +27,8 @@ def get_location(adresa):
         "krajina": odpoved.get("country_name")
     }
 
-
+    # Printneme odpoved:
+    print("\nPrintujem odpoved:")
     # Loopneme cez dict
     for key in location_data_dict:
         
@@ -29,6 +38,39 @@ def get_location(adresa):
         value = location_data_dict[key]
         
         print(kluc,"=",value)
+    
+    sys.exit()
+
+
+# +++ Funkcia pre overenie platnosti IP adresy
+def valid_ip_otaznik(ciselko):
+    
+    # Pomocou socket lib overime ipcku
+    try: 
+    
+        socket.inet_aton(ciselko)
+        # Returtneme, ze je platna
+        return True
+    
+    except socket.error:
+    
+        # Returneme, ze je neplatna, lebo IPcka nie je platna
+        return False
+
+
+IP = False
+
+# Kym nie je IPcka platna tak budeme loopovat input
+while not IP:
+
+    # Zadefinujeme odpoved ako input
+    Odpoved = input("Zadaj IP adresu pre jej lookup, ip adresa MUSI byt platna!\nAdresa: ")
+    
+    # Overime platnost ipcky funkciu valid_ip_otaznik
+    IP = valid_ip_otaznik(Odpoved)
 
 # Zavolame funkciu so zadanou ipckou
-get_location("212.37.87.11")
+get_location(Odpoved)
+
+
+# Testovacia ipcka --> 212.37.87.11
