edfc2edeece33b0bbe0f0a3efd23c54c97a8288e
david <89749147+analnyroztahovac@users.noreply.github.com>
2022-10-19 07:24:54 +0200

    Update - Misc fix

--- 3. Rocnik/Cvicenia/18-10-2022.py
+++ 3. Rocnik/Cvicenia/18-10-2022.py
@@ -1,21 +1,29 @@
+# Cvicenie poskytnute Alexom, yay
 import random
 import tkinter
 
 canvas=tkinter.Canvas()
 canvas.pack()
 
-# Vytvorte program, ktorý importne z txt súboru farby a vyfarbí 10 štvorčekov ľubovolnej velkosti, ktoré si vytvoríte. :)
 
 def prva_uloha():
-
+    
+    # Zadanie: Vytvorte program, ktory importne z txt suboru farby
+    # a vyfarbi na zaklade toho 10 stvorcekov. Kazda farba je
+    # oddelena medzerou.
+    
+    # Variability pre ulozenie pozicie
+    # zakladneho (prveho) stvorca 
     x = 30
     y = 30
-
+    
+    # Otvorenie suboru a jeho citanie
     subor = open("farbicky.txt", "r")
     exercise = subor.readline()
     
     color = exercise.split()
     
+    # Nacitanie stvorcekov
     for farbicka in range(10):
         canvas.create_rectangle(x, y, x + 30, y + 30, fill = color[farbicka])
         x += 30
@@ -23,17 +31,20 @@ def prva_uloha():
 #prva_uloha()
 
 
-# Ak si ma vysmial za zapísanie 1 úlohy. Tak zaprvé kys a podruhé kys. Vytvorte program, ktorý vykreslí 10 kruhov z txt súboru
-# v ktorom budete mať zadané údaje(x pozícia, y pozícia, polomer, farbička).
-
 def druha_uloha():
     
+    # Zadanie: Vytvorte program, ktory vykresli 10 kruhov
+    # podla zadanych udajov v textovom subore.
+    # Udaje: [pozicia_x] [pozicia_y] [polomer] [farba]
+    
+    # Otvorenie suboru a nacitanie jeho udajov
     subor = open("kruh.txt", "r")
-    kruh = subor.readline()
-            
-    while kruh != "":
-        kruh2 = kruh.split()
-        canvas.create_oval(kruh2[0], kruh2[1], int(kruh2[0]) + int(kruh2[2]), int(kruh2[1]) + int(kruh2[2]), fill = kruh2[3])    
-        kruh = subor.readline()
+    riadok = subor.readline()
+    
+    while riadok != "":
+        kruh = riadok.split()
+        canvas.create_oval(kruh[0], kruh[1], int(kruh[0]) + int(kruh[2]), int(kruh[1]) + int(kruh[2]), fill = kruh[3])    
+        # Nacitanie dalsieho riadka
+        riadok = subor.readline()
         
 #druha_uloha()
