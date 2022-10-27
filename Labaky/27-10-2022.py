b8f554c2410fddb190fea64c642a6393d19c54aa
david <89749147+analnyroztahovac@users.noreply.github.com>
2022-10-27 20:28:10 +0200

    labaky folder, cvicenie, readme

---
+++ Labaky/27-10-2022.py
@@ -0,0 +1,64 @@
+# Cvicenia z labakov.
+# Den - 27.10.2022 ; Skupina - I
+
+# +++> Info:
+# 
+#  - Delenie integerov a floatov v pythone pomocou operatora '/'
+#    returne VZDY float typ.
+#    @ Priklad:
+#       8 / 4 => 2.0
+# 
+# -  Delenie integerov a floatov v pythone pomocou operatora '//'
+#    zaokruhli vysledok a teda returne VZDY integer typ.
+#    @ Priklad:
+#      8 // 3 => 2
+# 
+#  - V Pythone existuje operator nazyvany ako "Modulo Operator",
+#    ktory nam zobrazi zvysok po deleni celych cisel. Znak: '%'
+#    @ Priklad:
+#       10 % 4 => 2
+#       Vysledok je 2, pretoze ak delime 10ku cislom 4 ostane nam zvysok 2.
+
+
+
+# ++ Zadanie k prvemu cviceniu:
+#   Napis program, ktory nahradi "Modulo Operator"
+#   Vstup nacitavaj pomocou input funkcie.
+
+# ++ Obmedzenia:
+#   Nesmies pouzit samotny Modulo Operator ( % )
+
+# Mozne riesenie:
+def prveCvicenie():
+    
+    prveCislo, druheCislo = int(input("Zadaj prve cislo: ")), int(input("Zadaj druhe cislo: "))
+    
+    return (prveCislo - ((prveCislo // druheCislo) * druheCislo))
+
+print(prveCvicenie())
+
+
+
+# ++ Zadanie k prvemu cviceniu:
+#   Napis program, ktory nahradi "Modulo Operator"
+#   Vstup nacitavaj pomocou input funkcie.
+
+# ++ Obmedzenia:
+#   Nesmies pouzit samotny Modulo Operator ( % )
+#   Nesmies pouzit delenie ( '/' ani '//' )
+
+# Mozne riesenie:
+def druheCvicenie():
+
+    prveCislo, druheCislo = int(input("Zadaj prve cislo: ")), int(input("Zadaj druhe cislo: "))
+
+    count, zakladneCislo = 0, prveCislo
+
+    while prveCislo >= druheCislo:
+
+        prveCislo -= druheCislo
+        count += 1
+    
+    return zakladneCislo - ( druheCislo * count )
+
+print(druheCvicenie())
---
+++ Labaky/README.md
@@ -0,0 +1,9 @@
+## Labaky:
+
+Tento folder sluzi prevazne ako zbierka cviceni, ktore sme robili na labakoch.</br>
+Cvicenia su pomenovane podla datumu ich konania. ( *dd-MM-yyyy* )</br>
+Cvicenia obsahuju zadanie ako aj *mozne* riesenie, samozrejme takmer vzdy sa stane, ze existuje viacero rieseni.</br>
+Moje riesenia nemusia byt vzdy uplne najspravnejsie, ucim sa tak ako aj vy. Pokial si vsak niekde vsimnete nejaku chybu pokojne
+mi ju nahlaste na discorde alebo priamo Pull-Requestnite jej opravu.</br>
+**V neposlednom rade chcem v mene nas vsetkych podakovat Filipovi, ze vyuziva svoj volny cas pre to aby nas nieco naucil.**
+
