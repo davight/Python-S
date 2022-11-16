5c806f8540c8ec48c26c36146a0eb3089f57b8af
david <89749147+analnyroztahovac@users.noreply.github.com>
2022-11-16 12:01:17 +0100

    update

--- 3. Rocnik/Uciva/list.py
+++ 3. Rocnik/Uciva/list.py
@@ -1,5 +1,5 @@
 # funkcie:
-# pop, append, extend, remove, count, find, sort, clear, insert, reverse
+# count, find, sort, reverse
 
 
 # ++> Funkcie append(), extend(), insert()
@@ -10,6 +10,7 @@
 #  Funkcia append() pridava do listu prave
 #  jeden item a to prave na posledne miesto v
 #  liste. Na type dat, ktore vkladame nezalezi.
+#  @ Format pisania: list.append([item])
 
 # ++> Funkcia extend()
 #  Funkcia extend() pridava (rozsiruje list)
@@ -18,14 +19,14 @@
 #  rozsirit list stringom, string vlozi ako
 #  list charakterov. Item sa vzdy vlozi
 #  prave na posledne miesto v liste.
+#  @ Format pisania: list.extend([item])
 
 # ++> Funkcia insert()
 #  Funkcia insert() je podobna funkcii append()
 #  ale hlavny rozdiel je v tom, ze v tejto funkcii
 #  musime specifikovat, na ktore miesto item chceme
-#  vlozit. Format pisania je: list.append(index, item)
-#  Pricom index predstavuje poziciu, na ktoru chceme
-#  item vlozit.
+#  vlozit. 
+#  @ Format pisania: list.append([index], [item])
 
 
 def prikladJeden():
@@ -35,15 +36,15 @@ def prikladJeden():
 
     # @@@ Vkladanie do listu pomocou append()
 
-    prveVlozenie = [1, 2, 3, 4, 'a', 'b', 'c', "Alex", "Filip", "Marko"]
-    prveVlozenie.append(1)
+    prveVlozenie = zakladnyList.copy()
+    #prveVlozenie.append(1)
     print(f"Prva uprava pomocou funkcie append: {prveVlozenie}")
 
-    druheVlozenie = [1, 2, 3, 4, 'a', 'b', 'c', "Alex", "Filip", "Marko"]
+    druheVlozenie = zakladnyList.copy()
     druheVlozenie.append("Mario")
     print(f"Druha uprava pomocou funkcie append: {druheVlozenie}")
 
-    tretieVlozenie = [1, 2, 3, 4, 'a', 'b', 'c', "Alex", "Filip", "Marko"]
+    tretieVlozenie = zakladnyList.copy()
     tretieVlozenie.append(["Foo", "Bar"])
     print(f"Tretie uprava pomocou funkcie append: {tretieVlozenie}")
     # Mozeme si vsimnut, ze list, ktory sme vlozili do zakladnehoListu
@@ -51,15 +52,15 @@ def prikladJeden():
 
     # @@@ Vkladanie do listu pomocou extend()
 
-    stvrteVlozenie = [1, 2, 3, 4, 'a', 'b', 'c', "Alex", "Filip", "Marko"]
+    stvrteVlozenie = zakladnyList.copy()
     stvrteVlozenie.extend(["Foo", "Bar"])
     print(f"Prva uprava pomocou funkcie extend: {stvrteVlozenie}")
 
-    piateVlozenie = [1, 2, 3, 4, 'a', 'b', 'c', "Alex", "Filip", "Marko"]
+    piateVlozenie = zakladnyList.copy()
     piateVlozenie.extend([1, 2])
     print(f"Druha uprava pomocou funkcie extend: {piateVlozenie}")
 
-    siesteVlozenie = [1, 2, 3, 4, 'a', 'b', 'c', "Alex", "Filip", "Marko"]
+    siesteVlozenie = zakladnyList.copy()
     siesteVlozenie.extend("AHOJ")
     print(f"Tretia uprava pomocou funkcie extend: {siesteVlozenie}")
     # Mozeme si vsimnut, ze sa string AHOJ vlozil do listu ako
@@ -67,17 +68,67 @@ def prikladJeden():
 
     # @@@ Vkladanie do listu pomocou insert()
 
-    siedmeVlozenie = [1, 2, 3, 4, 'a', 'b', 'c', "Alex", "Filip", "Marko"]
+    siedmeVlozenie = zakladnyList.copy()
     siedmeVlozenie.insert(2, "Cicky")
     print(f"Prva uprava pomocou funkcie insert: {siedmeVlozenie}")
 
-    osmeVlozenie = [1, 2, 3, 4, 'a', 'b', 'c', "Alex", "Filip", "Marko"]
+    osmeVlozenie = zakladnyList.copy()
     osmeVlozenie.insert(10, 1)
     print(f"Druha uprava pomocou funkcie insert: {osmeVlozenie}")
 
-    deviateVlozenie = [1, 2, 3, 4, 'a', 'b', 'c', "Alex", "Filip", "Marko"]
+    deviateVlozenie = zakladnyList.copy()
     deviateVlozenie.insert(-2, "Macicky")
     print(f"Tretia uprava pomocou funkcie insert: {deviateVlozenie}")
 
-prikladJeden()
+#prikladJeden()
 
+# ++> Funkcia remove()
+#  Odstrani prvy vyskyt itemu z listu,
+#  pokial tam item nie je vyhodi chybu.
+#  @ Format pisania: list.remove([item])
+
+# ++> Funkcia pop()
+#  Odstrani z listu item na zaklade pozicie.
+#  Pokial nezadas ziadnu poziciu vyhodi prave
+#  posledny item z listu. Pokial zadas
+#  poziciu mimo rangu listu vyhodi error.
+#  @ Format pisania: list.pop(index)
+#  + Tip:
+#     Indexovanie zacina na 0.
+
+# ++> Funkcia clear()
+#  Vycisti list, bude prazdny, empty...
+#  Format pisania je: list.clear()
+
+def prikladDva():
+    
+    zakladnyList = [1, 2, 3, 4, 'q', 'r']
+    print(f"Zakladny list: {zakladnyList}\n")
+
+    # @@@ Mazanie pomocou remove()
+
+    prveMazanie = zakladnyList.copy()
+    prveMazanie.remove(1)
+    print(f"Prva uprava pomocou funkcie remove: {prveMazanie}")
+
+    druheMazanie = zakladnyList.copy()
+    druheMazanie.remove('r')
+    print(f"Druha uprava pomocou funkcie remove: {druheMazanie}")
+    
+    # @@@ Mazanie pomocou pop()
+
+    tretieMazanie = zakladnyList.copy()
+    tretieMazanie.pop(2)
+    print(f"Prva uprava pomocou funkcie pop: {tretieMazanie}")
+
+    stvrteMazanie = zakladnyList.copy()
+    stvrteMazanie.pop()
+    print(f"Druha uprava pomocou funkcie pop: {stvrteMazanie}")
+
+    # @@@ Mazanie pomocou clear()
+
+    piateMazanie = zakladnyList.copy()
+    piateMazanie.clear()
+    print(f"Prva uprava pomocou funckei clear: {piateMazanie}")
+
+prikladDva()
