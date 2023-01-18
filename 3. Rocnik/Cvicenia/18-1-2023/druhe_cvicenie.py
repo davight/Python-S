55b86749116a35a0e4196d279d8151dce92635a7
david <89749147+analnyroztahovac@users.noreply.github.com>
2023-01-18 08:21:14 +0100

    cvicenie

---
+++ 3. Rocnik/Cvicenia/18-1-2023/druhe_cvicenie.py
@@ -0,0 +1,34 @@
+# Zadanie:
+#  Po kliknuti mysky vykresli ciaru od posledneho kliku
+#  pricom pri pohybe myskou sa musi zobrazovat ciara podla
+#  polohy mysky.
+
+import tkinter as tk
+ca = tk.Canvas()
+ca.pack()
+
+suradnice_move = None
+line = ca.create_line(0, 0, 0, 0)
+suradnice_click = []
+
+def click(m):
+    global suradnice_move, suradnice_click, line
+    
+    suradnice_move = m.x, m.y
+    line = ca.create_line(0, 0, 0, 0)
+    suradnice_click.extend([suradnice_move])
+    ca.coords(suradnice_click)
+
+def move(m):
+    global suradnice_move, line
+    
+    if suradnice_move is None: return
+    
+    ca.delete(line)
+    ca.update()
+    line = ca.create_line(suradnice_move, m.x, m.y)
+
+ca.bind("<Motion>", move)
+ca.bind("<Button-1>", click)
+
+ca.mainloop()
