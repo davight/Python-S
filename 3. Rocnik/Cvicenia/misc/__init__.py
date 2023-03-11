2a24ffd8ed542d9e55acfe6a7250da21dc700158
david <89749147+analnyroztahovac@users.noreply.github.com>
2023-03-11 23:04:48 +0100

    add misc funkciu

--- 3. Rocnik/Cvicenia/misc/__init__.py
+++ 3. Rocnik/Cvicenia/misc/__init__.py
@@ -9,3 +9,18 @@ def n_uholnik(pocet_uhlov: int, velkost: int, objekt) -> None:
     for _ in range(pocet_uhlov):
         objekt.left(uhol)
         objekt.fd(velkost)
+
+# @Nazov:
+# are_integers
+# @Popis:
+# returne boolean na zaklade toho
+# ci je hodnota list a zaroven ci
+# je mozne ho premenit na integery
+def are_integers(arr) -> bool:
+    if not isinstance(arr, list): return False
+    try:
+        for i in arr: int(i)
+    except ValueError:
+        return False
+    else:
+        return True
